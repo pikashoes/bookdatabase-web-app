b33c356a20c750cbca5a8b9cb2f1502c26cd8ed6
Jeff Cohen <cohen.jeff@gmail.com>
2017-07-05 00:00:54 -0500

    Simpler stack trace output

--- config/initializers/backtrace_silencers.rb
+++ config/initializers/backtrace_silencers.rb
@@ -5,3 +5,11 @@
 
 # You can also remove all the silencers if you're trying to debug a problem that might stem from framework code.
 # Rails.backtrace_cleaner.remove_silencers!
+
+module ActionDispatch
+  class ExceptionWrapper
+    def framework_trace
+      []
+    end
+  end
+end
